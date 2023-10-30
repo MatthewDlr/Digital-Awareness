c89b400a9f048d957e567c3102e3f77a2475bde5
Matthieu Delarue <matthieu.delarue5@gmail.com>
2023-10-30 21:25:01 +0100

    testing service worker registration

--- src/background.js
+++ src/background.js
@@ -1,1 +1,16 @@
-console.log("This prints to the console of the service worker (background script)")
+console.log("Service worker works!")
+
+chrome.webNavigation.onCompleted.addListener(function(details) {
+    let hostname = new URL(details.url).hostname;
+
+    if (hostname) {
+        console.log("Navigated to: " + hostname);
+    }
+
+    if (hostname == "www.youtube.com") {
+        console.log("GOTCHA");
+        let redirectUrl = chrome.runtime.getURL('index.html#popup');
+        chrome.tabs.update(details.tabId, {url: redirectUrl});
+    }
+}); 
+
