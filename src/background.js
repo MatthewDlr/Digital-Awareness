46912ae621b3b80036a628d6b8862852386b1f35
Matthieu Delarue <matthieu.delarue5@gmail.com>
2023-11-01 10:54:14 +0100

    Added blocklist

--- src/background.js
+++ src/background.js
@@ -1,16 +1,49 @@
-console.log("Service worker works!")
+chrome.webNavigation.onCompleted.addListener(function (details) {
+  if (details.frameId != 0) {
+    return;
+  }
 
-chrome.webNavigation.onCompleted.addListener(function(details) {
-    let hostname = new URL(details.url).hostname;
+  let host = new URL(details.url).host;
+  if (host.substring(0, 3) == "www") host = host.substring(4);
 
-    if (hostname) {
-        console.log("Navigated to: " + hostname);
+  chrome.storage.sync.get("blocketWebsites", function (result) {
+    let blocketWebsites = result.blocketWebsites;
+
+    if (blocketWebsites.includes(host)) {
+      console.log("Gotcha! ", host);
+      let redirectUrl = chrome.runtime.getURL("index.html#popup");
+      chrome.tabs.update(details.tabId, { url: redirectUrl });
     }
+  });
+});
 
-    if (hostname == "www.youtube.com") {
-        console.log("GOTCHA");
-        let redirectUrl = chrome.runtime.getURL('index.html#popup');
-        chrome.tabs.update(details.tabId, {url: redirectUrl});
-    }
-}); 
+chrome.runtime.onInstalled.addListener(() => {
+  writeDefaultConfig();
+});
 
+function writeDefaultConfig() {
+  chrome.storage.sync
+    .set({
+      blocketWebsites: [
+        "x.com",
+        "twitter.com",
+        "youtube.com",
+        "facebook.com",
+        "instagram.com",
+        "reddit.com",
+        "tiktok.com",
+        "netflix.com",
+        "twitch.tv",
+        "pinterest.com",
+        "tumblr.com",
+        "snapchat.com",
+        "aliexpress.com",
+        "alibaba.com",
+        "wish.com",
+        "etsy.com",
+      ],
+    })
+    .then(() => {
+      console.log("Default settings applied");
+    });
+}
