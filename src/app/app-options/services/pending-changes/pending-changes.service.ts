8e30dda7f87d86dccd0cea1fa91e6516065866f1
Matthieu <matthieu.delarue5@gmail.com>
2024-02-07 13:49:32 +0100

    fix prettier installation & cleanup dependencies

--- src/app/app-options/services/pending-changes/pending-changes.service.ts
+++ src/app/app-options/services/pending-changes/pending-changes.service.ts
@@ -40,7 +40,7 @@ export class PendingChangesService {
     if (this.websitesToDelete.has(host)) {
       return true;
     }
-    // @ts-ignore: Not all code paths return a value.
+    // @ts-expect-error: Not all code paths return a value.
     this.websitesToEdit.forEach(website => {
       if (website.oldHost === host || website.newHost === host) {
         return true;
