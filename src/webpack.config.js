5b4ff54eff09fca84b0772a43176fc38613f43d7
Matthieu <matthieu.delarue5@gmail.com>
2024-11-10 21:34:57 -0800

    🏗️ Move to Angular new build system

---
+++ src/webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: {
+    serviceWorker: {
+      import: "./src/service-worker.ts",
+      runtime: false,
+    },
+    doomScrolling: {
+      import: "./src/scripts/doomScrolling/doomScrolling.script.ts",
+      runtime: false,
+    },
+    bedtimeMode: {
+      import: "./src/scripts/bedtimeMode/bedtimeMode.script.ts",
+      runtime: false,
+    },
+  },
+  optimization: {
+    runtimeChunk: false,
+  },
+};
