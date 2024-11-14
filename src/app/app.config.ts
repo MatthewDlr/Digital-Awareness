3555ae9cdab1473e4fe10a00efbb9af6d8abf915
Matthieu <matthieu.delarue5@gmail.com>
2024-11-13 19:07:35 -0800

    💫 Add back animations module

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { routes } from "./app.routes";
 import { HashLocationStrategy, LocationStrategy } from "@angular/common";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideAnimationsAsync(),
     { provide: LocationStrategy, useClass: HashLocationStrategy },
   ],
 };
